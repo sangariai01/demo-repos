6e1c70fcdd4920783569277d99947b0b74e219e4
sangarisai <sangarisai01@gmail.com>
2024-01-04 10:52:30 +0530

    add elseif file

---
+++ elseif.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main()
+ {
+    int a;
+    printf("The given number is:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("The positive number ");
+    }
+    else
+    {
+      printf("The negative number ");
+    }
+    return 0;
+ }
